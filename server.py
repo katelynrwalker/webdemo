2910b5e762e545da2825fec950537584b05b84e0
Katelyn Walker <walker.katelyn@gmail.com>
2018-10-31 13:19:30 -0700

    with plotting

--- server.py
+++ server.py
@@ -14,5 +14,26 @@ def home():
 def mpg():
     return render_template('mpg.html')
 
+#load in the model (created and pickled from a seperate python file - don't need
+# to call that here though because the model itself is pickled)
+model = pickle.load(open('linreg.p', 'rb'))
+
+#create an inference route
+#GET method 
+@app.route('/inference', methods=['POST'])
+def inference():
+    req = request.get_json()
+    print(req)
+    c,h,w = req['cylinders'],req['horsepower'],req['weight']
+    prediction = list(model.predict([[c,h,w]]))
+    return jsonify({'c':c, 'h':h, 'w':w, 'prediction':prediction})
+
+#Create a route for plotting
+@app.route('/plot', methods = ['GET'])
+def plot():
+    df = pd.read_csv('cars.csv')
+    data = list(zip(df.mpg, df.weight))
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 3333, debug = True)
--- static/mpg.js
+++ static/mpg.js
@@ -12,5 +12,42 @@ $(document).ready(function(){
       weight
     }
     console.log(data)
+    const response = await $.ajax('/inference',{
+      data: JSON.stringify(data),
+      method: "post",
+      contentType: "application/json"
+    })
+    console.log(response)
+    $('#mpg').val(response.prediction)
+  })
+
+  $('#scatter-plot').click(async function(){
+    console.log('scatter button was clicked')
+    const response = await $.ajax('/plot')
+    console.log(response)
+    const mpg = response.map(a => a[0])
+    const weight = response.map(a => a[1])
+    console.log(mpg)
+
+    //Create the trace
+    const trace = [{
+      x:weight,
+      y:mpg,
+      mode:"markers",
+      type:"scatter"
+    }]
+
+    //Create the layout
+    const layout = {
+      xaxis:{
+        title:'Weight'
+      },
+      yaxis:{
+        title:"MPG"
+      },
+      title: "Scatter MPG vs. Weight",
+      width:700,
+      height:300
+    }
   })
 })
