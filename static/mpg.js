492e55dec3e55a7c8ecdc9cc971c4f673120bde0
Katelyn Walker <walker.katelyn@gmail.com>
2018-10-31 10:45:26 -0700

    with javascript

---
+++ static/mpg.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  console.log('document is ready')
+
+  $('#inference').click(async function(){
+    console.log('button was clicked')
+    const cylinders = parseFloat($('#cylinders').val());
+    const horsepower = parseFloat($('#horsepower').val());
+    const weight = parseFloat($('#weight').val());
+    const data = {
+      cylinders,
+      horsepower,
+      weight
+    }
+    console.log(data)
+  })
+})
