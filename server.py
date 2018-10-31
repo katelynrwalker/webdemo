d8a83c16725b0d19b6cda7260d1db504fc76435c
Katelyn Walker <walker.katelyn@gmail.com>
2018-10-31 10:24:06 -0700

    more webpage building

--- server.py
+++ server.py
@@ -4,11 +4,15 @@ import pandas as pd
 
 app = Flask(__name__)
 
+#create the home page
 @app.route('/', methods = ['GET'])
-
 def home():
     return render_template('home.html')
 
+#create the mpg page
+@app.route('/mpg', methods = ['GET'])
+def mpg():
+    return render_template('mpg.html')
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port = 3333, debug = True)
